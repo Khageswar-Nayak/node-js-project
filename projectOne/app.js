2f2a931bfc0890b493ff21758069f85393a5e51e
Khageswar-Nayak <khageswarnayak045@gmail.com>
2023-11-17 00:15:36 +0530

    Intro to Middlewares

--- projectOne/app.js
+++ projectOne/app.js
@@ -4,13 +4,24 @@
 const express = require("express");
 const app = express();
 
-app.use((req, res, next) => {
-  console.log("first middleware");
-  next(); // allows the request to continue to the next middleware in line
+// for parsing the incoming request body we have to install third party package like body-parser
+
+const bodyParser = require("body-parser");
+
+app.use(bodyParser.urlencoded({ extended: false }));
+
+app.use("/add-product", (req, res, next) => {
+  res.send(
+    "<form action='/product' method='POST'><input type='text' name='title' /><input type='text' name='size' /><button type='submit'>Add Product</button></form>"
+  );
 });
 
-app.use((req, res, next) => {
-  console.log("second middleware");
+app.use("/product", (req, res, next) => {
+  console.log(req.body); //it gives undefined if we don't install third party package
+  res.redirect("/"); // we can redirect to any path using redirect function
+});
+
+app.use("/", (req, res, next) => {
   res.send("<h1>Hello From Express ! </h1>");
 });
 

cd4e8ad6963e5bb9c1dbd3dab7f0684d83fb871b
Khageswar-Nayak <khageswarnayak045@gmail.com>
2023-11-17 09:30:04 +0530

    Routers and Filters

--- projectOne/app.js
+++ projectOne/app.js
@@ -4,25 +4,20 @@
 const express = require("express");
 const app = express();
 
+const adminRoutes = require("./routes/admin");
+const shopRoutes = require("./routes/shop");
+
 // for parsing the incoming request body we have to install third party package like body-parser
 
 const bodyParser = require("body-parser");
 
 app.use(bodyParser.urlencoded({ extended: false }));
 
-app.use("/add-product", (req, res, next) => {
-  res.send(
-    "<form action='/product' method='POST'><input type='text' name='title' /><input type='text' name='size' /><button type='submit'>Add Product</button></form>"
-  );
-});
+app.use("/admin", adminRoutes);
+app.use(shopRoutes);
 
-app.use("/product", (req, res, next) => {
-  console.log(req.body); //it gives undefined if we don't install third party package
-  res.redirect("/"); // we can redirect to any path using redirect function
-});
-
-app.use("/", (req, res, next) => {
-  res.send("<h1>Hello From Express ! </h1>");
+app.use((req, res, next) => {
+  res.status(404).send("<h1>Page not found</h1>");
 });
 
 app.listen(4000);
